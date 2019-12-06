ac573370e5f34862de52ce95aab80cf688590201
Carl E Leiby <carl_leiby@cable.comcast.com>
2019-12-06 08:56:05 -0500

    some event work

--- lib/barcamp_web/controllers/schedule/event_controller.ex
+++ lib/barcamp_web/controllers/schedule/event_controller.ex
@@ -20,6 +20,7 @@ defmodule BarcampWeb.Schedule.EventController do
         conn
         |> put_flash(:info, "Event created successfully.")
         |> redirect(to: schedule_event_path(conn, :index))
+
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
@@ -29,4 +30,18 @@ defmodule BarcampWeb.Schedule.EventController do
     event = Schedule.get_event!(id)
     render(conn, "show.html", event: event)
   end
+
+  def edit(conn, %{"id" => id}) do
+    {id, _} = Integer.parse(id)
+    event = Schedule.get_event!(id)
+    render(conn, "edit.html", event: event, changeset: Event.changeset(event, %{}))
+  end
+
+  # def update(conn, %{"id" => id, "quote" => %{"saying" => saying, "author" => author}}) do
+  #   {id, _} = Integer.parse(id)
+  #   q = Repo.get(Splurty.Quote, id)
+  #   q = %{q | saying: saying, author: author }
+  #   Repo.update(q)
+  #   redirect conn, to: quote_path(conn, :show, q.id)
+  # end
 end
--- lib/barcamp_web/views/schedule/event_view.ex
+++ lib/barcamp_web/views/schedule/event_view.ex
@@ -2,5 +2,4 @@ defmodule BarcampWeb.Schedule.EventView do
   use BarcampWeb, :view
 
   import BarcampWeb.LayoutView, only: [current_user: 1]
-  
 end
